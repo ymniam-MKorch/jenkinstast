2a6fc13b618d5b420f5dfaeff050a23c1acad425
sunwen <sunwen@citrusjoy.com>
2020-09-27 09:58:42 +0800

    test

---
+++ Controllers/TestController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using test.Models;
+
+namespace test.Controllers
+{
+    [Route("[controller]")]
+    public class TestController : Controller
+    {
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpGet("hello")]
+        public string HelloWorld()
+        {
+            return "Hello World!";
+        }
+    }
+}
